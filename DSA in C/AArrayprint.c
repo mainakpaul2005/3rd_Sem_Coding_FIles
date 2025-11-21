7b144a751a90f33cd187fd2b491f3be211912cce
Mainak Paul <mainakpaul2005@gmail.com>
2025-11-21 14:17:15 +0530

    Additions to DSA and PYthon Lab Work

--- DSA in C/AArrayprint.c
+++ DSA in C/AArrayprint.c
@@ -1,27 +1,18 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int main()
-{
+int main(){
 	int* arr;
 	int size,i;
-	
-	printf("ENter the no of numbers : ");
+	printf("Enter the no of numbers : ");
 	scanf("%d",&size);
-	
-	arr = (int* ) malloc(size * sizeof(int));
-	
+	arr = (int*) malloc(size * sizeof(int));
 	printf("Enter the numbers: ");
-	
-	for( i = 0; i < size; i++)
-	{
+	for(i=0;i<size;i++){
 		scanf("%d",&arr[i]);
 	}
-	
 	printf("The Entered Numbers are : ");
-	
-	for( i = 0 ; i < size; i++)
-	{
+	for(i=0;i<size;i++){
 		printf(" %d ",arr[i]);
 	}
 	
