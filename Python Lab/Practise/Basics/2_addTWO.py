157a53307cf69c7cfb53352421dbcab84f1b9c4c
Mainak Paul <mainakpaul2005@gmail.com>
2026-02-03 03:29:03 +0530

    Added Python Lab/Practise/

---
+++ Python Lab/Practise/Basics/2_addTWO.py
@@ -0,0 +1,5 @@
+a = int(input("Enter 1st number: "))
+b = int(input("Enter 2nd number: "))
+
+sum = a + b
+print(f"The sum of {a} and {b} is {sum}")
---
+++ Python Lab/Practise/Basics/3_areaCIRCLE.py
@@ -0,0 +1,4 @@
+pi = 3.14
+r = float(input("Enter the radius of the circle: "))
+area = pi * r * r
+print(f"Area of a circle with radius {r} is {area}")
